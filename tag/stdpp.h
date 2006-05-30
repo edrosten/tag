bef800da96886fb118b9e20731bff6b294b97cbb
gerhard <gerhard>
2006-05-30 19:53:22 +0000

    documentation fixes

--- tag/stdpp.h
+++ tag/stdpp.h
@@ -8,24 +8,27 @@ namespace tag {
 /// @defgroup stdpp C++ std lib enhancements
 /// This group contains enhancements to make the std library more useable
 
-/// @defgroup print stream simplifications
-/// The comma operator for streams is defined to allow a simple statement similar to print in Python
-/// or other languages. The stream object and any values to be printed are written as a comma separated
-/// list. Between the values printed, the stream's fill character is output to delimit values. Some examples:
-/// @code
-/// cout, 12, "hello", 13, endl;
-/// cout, setfill('|');
-/// cout, 12, 13, 14, endl;
-/// @endcode
-/// will result in the following output.
-/// @code
-/// 12 hello 13
-/// 12|13|14
-/// @endcode
-/// endl is treated specially and no separator is produced before or after it. Other iomanips are not
-/// treated specially and produce a fill character before their position (if they are not the first in the list).
-/// @ingroup stdpp
-/// @{
+/**
+@defgroup print stream simplifications
+The comma operator for streams is defined to allow a simple statement similar to print in Python
+or other languages. The stream object and any values to be printed are written as a comma separated
+list. Between the values printed, the stream's fill character is output to delimit values. Some examples:
+@code
+cout, 12, \"hello\", 13, endl;
+cout, setfill('|');
+cout, 12, 13, 14, endl;
+@endcode
+will result in the following output
+@code
+12 hello 13
+12|13|14
+@endcode
+endl is treated specially and no separator is produced before or after it. Other iomanips are not
+treated specially and produce a fill character before their position (if they are not the first in the list).
+@ingroup stdpp
+*/
+
+#ifndef DOXYGEN_IGNORE_INTERNAL
 
 template <class T> struct NotFirst {
     inline NotFirst(T & d) : data(d) {};
@@ -55,10 +58,7 @@ inline O & operator,(NotFirst<O> nf, O & (*modifier)(O &)){
     nf.data << modifier;
     return nf.data;
 }
-/// @}
 
-
-#ifndef DOXYGEN_IGNORE_INTERNAL
 namespace Internal
 {
 
@@ -120,47 +120,51 @@ namespace Internal
 
 }
 
-#endif 
+#endif
 
-/// Ostream modifier which puts spaces between elements using the fill character. endl is treated
-/// specially, so a space is not put after an endl
-/// @code
-///	cout << add_fill << 1 << 2 << 3 << endl << 4  << 5 << endl;
-/// @endcode
-/// This will print:
-/// @code
-/// 1 2 3
-/// 5 4
-/// @endcode
-/// @ingroup print
+/**
+Ostream modifier which puts spaces between elements using the fill character. endl is treated
+specially, so a space is not put after an endl
+@code
+cout << add_fill << 1 << 2 << 3 << endl << 4<< 5 << endl;
+@endcode
+This will print:
+@code
+1 2 3
+5 4
+@endcode
+@ingroup print
+*/
 static struct Internal::add_fill_s add_fill;
 
-/// Ostream modifier similar to @ref add_fill, except that an endl is added at the end automatically
-/// @code
-/// cout << print << 1 << 2 << 3;
-/// cout << "hello" << endl;
-/// @endcode
-/// Will print:
-/// @code
-/// 1 2 3
-/// hello
-/// @endcode
-/// @ingroup print
+/**
+Ostream modifier similar to @ref add_fill, except that an endl is added at the end automatically
+@code
+cout << print << 1 << 2 << 3;
+cout << "hello" << endl;
+@endcode
+Will print:
+@code
+1 2 3
+hello
+@endcode
+@ingroup print
+*/
 static struct Internal::like_print_s print;
 
-
-/// An ostream modifier to use with @ref print and @ref add_fill which prevents a space being added
-/// before the next element.
-/// @code
-/// cout << @ref add_fill  << 0 << 1 << no_space << 2 << 3 << endl;
-/// @endcode
-/// will print:
-/// @code
-/// 0 12 3
-/// @endcode
-/// @ingroup print
+/**
+An ostream modifier to use with @ref print and @ref add_fill which prevents a space being added
+before the next element.
+@code
+cout << @ref add_fill  << 0 << 1 << no_space << 2 << 3 << endl;
+@endcode
+will print:
+@code
+0 12 3
+@endcode
+@ingroup print
+*/
 static struct Internal::no_space_s no_space;
-
 
 #ifndef DOXYGEN_IGNORE_INTERNAL
 
