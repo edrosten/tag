b3e2bf91323557cd2d61b80fea50cb3cace1d998
edrosten <edrosten>
2006-05-30 18:47:46 +0000

    Added add_fill, print and no_space ostream modifiers for use with operator<<. For some reason documentation doesn't work.

--- tag/stdpp.h
+++ tag/stdpp.h
@@ -1,6 +1,8 @@
 #ifndef __PRINT_H_
 #define __PRINT_H_
 
+#include <iostream>
+
 namespace tag {
 
 /// @defgroup stdpp C++ std lib enhancements
@@ -53,8 +55,126 @@ inline O & operator,(NotFirst<O> nf, O & (*modifier)(O &)){
     nf.data << modifier;
     return nf.data;
 }
-
 /// @}
+
+
+#ifndef DOXYGEN_IGNORE_INTERNAL
+namespace Internal
+{
+
+	struct add_fill_s{};
+	struct like_print_s{};
+	struct no_space_s{};
+
+	template<class S> struct add_fill_bound
+	{
+		add_fill_bound(S& os)
+		:o(os),first(1) {}
+
+
+		template<class C> add_fill_bound& operator<<(const C& c)
+		{
+				if(first == true)
+					first=false;
+				else
+					o << o.fill();
+
+				o << c;
+				return *this;
+		}
+		
+		add_fill_bound& operator<<(const no_space_s&)
+		{
+			first=true;
+			return *this;
+		}
+		
+		add_fill_bound& operator<<(S& (*fptr)(S&) )
+		{
+				o << fptr;
+
+				if(fptr == static_cast<S&(*)(S&)>(std::endl))
+					first=true;
+
+				return *this;
+		}
+
+		private:
+			S& o;
+			bool first;
+	};
+
+
+	template<class S> struct like_print_bound:public add_fill_bound<S>
+	{
+		like_print_bound(S&os)
+		:add_fill_bound<S>(os)
+		{
+		}
+		
+		~like_print_bound()
+		{
+			add_fill_bound<S>::operator<<(std::endl);
+		}
+	};
+
+}
+
+#endif 
+
+/// Ostream modifier which puts spaces between elements using the fill character. endl is treated
+/// specially, so a space is not put after an endl
+/// @code
+///	cout << add_fill << 1 << 2 << 3 << endl << 4  << 5 << endl;
+/// @endcode
+/// This will print:
+/// @code
+/// 1 2 3
+/// 5 4
+/// @endcode
+/// @ingroup print
+static struct Internal::add_fill_s add_fill;
+
+/// Ostream modifier similar to @ref add_fill, except that an endl is added at the end automatically
+/// @code
+/// cout << print << 1 << 2 << 3;
+/// cout << "hello" << endl;
+/// @endcode
+/// Will print:
+/// @code
+/// 1 2 3
+/// hello
+/// @endcode
+/// @ingroup print
+static struct Internal::like_print_s print;
+
+
+/// An ostream modifier to use with @ref print and @ref add_fill which prevents a space being added
+/// before the next element.
+/// @code
+/// cout << @ref add_fill  << 0 << 1 << no_space << 2 << 3 << endl;
+/// @endcode
+/// will print:
+/// @code
+/// 0 12 3
+/// @endcode
+/// @ingroup print
+static struct Internal::no_space_s no_space;
+
+
+#ifndef DOXYGEN_IGNORE_INTERNAL
+
+template<class Char, class Traits> Internal::add_fill_bound<std::basic_ostream<Char,Traits> > operator<<(std::basic_ostream<Char,Traits>& o, const Internal::add_fill_s&)
+{
+		return Internal::add_fill_bound<std::basic_ostream<Char,Traits> >(o);
+}
+ 
+template<class Char, class Traits> Internal::like_print_bound<std::basic_ostream<Char,Traits> > operator<<(std::basic_ostream<Char,Traits>& o, const Internal::like_print_s&)
+{
+		return Internal::like_print_bound<std::basic_ostream<Char,Traits> >(o);
+}
+
+#endif
 
 } // namespace tag
 
