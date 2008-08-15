99a275e804843e1360ab660b7d87d9e8247f2b68
gerhard <gerhard>
2008-08-15 13:24:48 +0000

    added hand eye calibration routines

---
+++ src/handeye.cpp
@@ -0,0 +1,84 @@
+#include <tag/handeye.h>
+#include <tag/absorient.h>
+
+#include <TooN/Cholesky.h>
+
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+using namespace TooN;
+
+namespace tag {
+
+static inline Vector<3> getRotationVector( const SO3 & r ){
+    const Matrix<3> & my_matrix = r.get_matrix();
+    Vector<3> result;
+    result[0] = (my_matrix[2][1]-my_matrix[1][2]);
+    result[1] = (my_matrix[0][2]-my_matrix[2][0]);
+    result[2] = (my_matrix[1][0]-my_matrix[0][1]);
+    normalize(result);
+    return result;
+}
+
+static inline Vector<3> getRotationVector( const SE3 & t ){
+    return getRotationVector(t.get_rotation());
+}
+
+template<class T>
+static inline TooN::SO3 solveXABX( const std::vector<T> & A, const std::vector<T> & B ){
+    vector<Vector<3> > va(A.size()), vb(A.size());
+    for(unsigned int i = 0; i < A.size(); ++i){
+        va[i] = getRotationVector(A[i]);
+        vb[i] = getRotationVector(B[i]);
+    }
+    return computeOrientation(va, vb);
+}
+
+static inline Matrix<3> eyeMinus( const Matrix<3> & m ){
+    Matrix<3> result;
+    Identity(result);
+    result -= m;
+    return result;
+}
+
+SE3 computeHandEyeSingle( const vector<SE3> & AB, const vector<SE3> & CD ){
+    vector<SE3> A(AB.size()-1),B(AB.size()-1);
+    for(unsigned int i = 0; i < AB.size() - 1; ++i){
+        A[i] = CD[i] * CD[i+1].inverse();
+        B[i] = AB[i].inverse() * AB[i+1];
+    }
+    SO3 R = solveXABX(A,B);
+    Matrix<3> JTJ;
+    Vector<3> JTE;
+    Zero(JTJ);
+    Zero(JTE);
+    for(unsigned int i = 0; i < A.size(); ++i){
+        Matrix<3> m =  eyeMinus( B[i].get_rotation().get_matrix());
+        add_product(m.T(), m, JTJ);
+        add_product(m.T(), (B[i].get_translation() - R * A[i].get_translation()), JTE);
+    }
+    Cholesky<3> chol(JTJ);
+    return SE3(R, chol.inverse_times(JTE));
+}
+
+std::pair<TooN::SE3, TooN::SE3> computeHandEye( const std::vector<TooN::SE3> & AB, const std::vector<TooN::SE3> & CD ){
+    assert( AB.size() == CD.size() && AB.size() > 2 );
+    return make_pair(computeHandEyeSingle(AB, CD), computeHandEyeSingle(CD, AB)); 
+}
+
+SO3 computeHandEyeSingle( const vector<SO3> & AB, const vector<SO3> & CD ){
+    vector<SO3> A(AB.size()-1),B(AB.size()-1);
+    for(unsigned int i = 0; i < AB.size() - 1; ++i){
+        A[i] = CD[i] * CD[i+1].inverse();
+        B[i] = AB[i].inverse() * AB[i+1];
+    }
+    return solveXABX(A,B);
+}
+
+std::pair<TooN::SO3, TooN::SO3> computeHandEye( const std::vector<TooN::SO3> & AB, const std::vector<TooN::SO3> & CD ){
+    assert( AB.size() == CD.size() && AB.size() > 2 );
+    return make_pair(computeHandEyeSingle(AB, CD), computeHandEyeSingle(CD, AB));
+}
+
+}
---
+++ tag/handeye.h
@@ -0,0 +1,53 @@
+#ifndef TAG_HANDEYE_H_
+#define TAG_HANDEYE_H_
+
+#include <vector>
+#include <TooN/se3.h>
+
+namespace tag {
+
+/// @defgroup handeye Hand Eye calibration
+/// contains various functions to solve simultaneously for two transformations 
+/// a ring of transformations. For example, world - world and sensor - sensor
+/// registration for two sensors. The implementation follows roughly Tsai & Lenz 89.
+/// It is only a linear method and does not include any nonlinear optimisation.
+
+/// computes the transformation in a circle of transformations:
+/// AB -> X -> CD -> ?, so that X is the transformation from B to C. ? is not
+/// computed. AB, CD need to contain at least 3 entries to provide 2 independent motions
+/// @param[in] AB a vector of measurements for AB
+/// @param[in] CD a vector of measurements for CD
+/// @return the SE3 X 
+/// @ingroup handeye
+TooN::SE3 computeHandEyeSingle( const std::vector<TooN::SE3> & AB, const std::vector<TooN::SE3> & CD );
+
+/// computes the pair of transformations that complete the circle:
+/// AB -> X -> CD -> Y, so that X is the transformation from B to C and Y the 
+/// transformation from D to A. AB, CD need to contain at least 3 entries to provide 2 independent motions
+/// @param[in] AB a vector of measurements for AB
+/// @param[in] CD a vector of measurements for CD
+/// @return pair of SE3s, X and Y
+/// @ingroup handeye
+std::pair<TooN::SE3, TooN::SE3> computeHandEye( const std::vector<TooN::SE3> & AB, const std::vector<TooN::SE3> & CD );
+
+/// computes the rotation in a circle of transformations:
+/// AB -> X -> CD -> ?, so that X is the rotation from B to C. ? is not
+/// computed. AB, CD need to contain at least 3 entries to provide 2 independent motions
+/// @param[in] AB a vector of measurements for AB
+/// @param[in] CD a vector of measurements for CD
+/// @return the SE3 X 
+/// @ingroup handeye
+TooN::SO3 computeHandEyeSingle( const std::vector<TooN::SO3> & AB, const std::vector<TooN::SO3> & CD );
+
+/// computes the pair of rotations that complete the circle:
+/// AB -> X -> CD -> Y, so that X is the transformation from B to C and Y the 
+/// transformation from D to A. AB, CD need to contain at least 3 entries to provide 2 independent motions
+/// @param[in] AB a vector of measurements for AB
+/// @param[in] CD a vector of measurements for CD
+/// @return pair of SO3s, X and Y
+/// @ingroup handeye
+std::pair<TooN::SO3, TooN::SO3> computeHandEye( const std::vector<TooN::SO3> & AB, const std::vector<TooN::SO3> & CD );
+
+}
+
+#endif //  TAG_HANDEYE_H_
