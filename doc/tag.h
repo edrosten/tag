19b2dc381916df46aa933d2a85e5f9bdd90090ce
gerhard <gerhard>
2009-08-05 14:32:54 +0000

    docu on win32 build files

--- doc/tag.h
+++ doc/tag.h
@@ -34,12 +34,23 @@ To get the code from cvs use:
 cvs -z3 -d:pserver:anoncvs@cvs.savannah.nongnu.org:/cvsroot/toon co tag
 @endcode
 
+@subsection unix Unix
+
 On a unix system, <code>./configure && make install </code> will compile the %tag
 library and install it to the correct place. It creates dynamic and static libraries
 with the library name @b toontag.
 
+@subsection win32 Windows
+
 For Win32 systems, the @c build directory contains project files for different versions
-of Visual Studio.
+of Visual Studio. Currently the vc2008 solutin is supported and should work out of the box. There
+are two projects, one for compiling tag and one for installing it into a common directory tree.
+Both projects assume the existence of three environment variables describing the location of header, 
+library and binary files (for DLLs). 
+	
+	- @c INCLUDEDIR contains the header files. tag headers will be copied into @c \%INCLUDEDIR%\\\tag
+	- @c LIBDIR contains library files. tag static libraries (debug and release verions) will be copied into @c \%LIBDIR\%
+	- @c BINDIR is not used for tag, but would be the default directory for DLLs
 
 @section sLinks Links
     - TooN - http://mi.eng.cam.ac.uk/~twd20/TooN/html/index.html
